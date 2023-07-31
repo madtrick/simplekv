0e98aa64f3c510f4aea0fe7ca17904e2c4437a0a
Farruco Sanjurjo <madtrick@gmail.com>
2023-07-31 23:52:26 +0200

    Init KV from the log file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,58 @@
 use easy_repl::{command, CommandStatus, Repl};
-use std::io::Write;
+use regex::Regex;
+use std::io::{BufRead, BufReader, Write};
 use std::{cell::RefCell, collections::HashMap, fs::File};
 
-// fn upsert_logfile (filename: &str) {
-//
-//
-// }
-
-fn main() {
-    let values = RefCell::new(HashMap::new());
-    let values_ref = &values;
-    let mut file = File::options()
+fn upsert_logfile(filename: &str) -> File {
+    File::options()
         .append(true)
         .create(true)
-        .open("log")
-        .unwrap();
+        .open(filename)
+        .unwrap()
+}
+
+fn print_logfile(filename: &str) {
+    // TODO: I wanted to do `File::open(filename)?` but it won't compile. Why?
+    let file = File::open(filename).unwrap();
+    let lines = BufReader::new(file).lines();
+    let regex = Regex::new(r"(\w+)=(\w+)").unwrap();
+
+    lines.for_each(|line| {
+        if let Ok(line) = line {
+            let Some(capture) = regex.captures(line.as_str()) else { return };
+
+            println!("Key {}, Value {}", &capture[1], &capture[2])
+        }
+    })
+}
+
+fn init_from_logfile(filename: &str) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    let file = File::open(filename).unwrap();
+    let lines = BufReader::new(file).lines();
+    let regex = Regex::new(r"(\w+)=(\w+)").unwrap();
+
+    lines.for_each(|line| {
+        if let Ok(line) = line {
+            // This patter of `let x = foo else bar` is called let-else. https://rust-lang.github.io/rfcs/3137-let-else.html
+            let Some(capture) = regex.captures(line.as_str()) else { return };
+
+            // TODO: understand why I need to pass a reference to println
+            println!("Key {}, Value {}", &capture[1], &capture[2]);
+            map.insert(String::from(&capture[1]), String::from(&capture[2]));
+        }
+    });
+
+    map
+}
+
+fn main() {
+    let map = init_from_logfile("log");
+    let values = RefCell::new(map);
+    let values_ref = &values;
+    let mut file = upsert_logfile("log");
+
+    print_logfile("log");
 
     let mut repl = Repl::builder()
         .add(
