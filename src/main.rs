9b2587671c959f0f6262c97eaee7890c1c6c40c4
Farruco Sanjurjo <madtrick@gmail.com>
2023-08-01 21:50:52 +0200

    DEL values

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,21 @@ fn init_from_logfile(filename: &str) -> HashMap<String, String> {
     let mut map = HashMap::new();
     let file = File::open(filename).unwrap();
     let lines = BufReader::new(file).lines();
-    let regex = Regex::new(r"(\w+)=(\w+)").unwrap();
+    let set_regex = Regex::new(r"(\w+)=(\w+)").unwrap();
+    let del_regex = Regex::new(r"DEL (\w+)").unwrap();
 
     lines.for_each(|line| {
         if let Ok(line) = line {
             // This patter of `let x = foo else bar` is called let-else. https://rust-lang.github.io/rfcs/3137-let-else.html
-            let Some(capture) = regex.captures(line.as_str()) else { return };
+            if let Some(capture) = set_regex.captures(line.as_str()) {
+                println!("Key {}, Value {}", &capture[1], &capture[2]);
+                map.insert(String::from(&capture[1]), String::from(&capture[2]));
+            } else if let Some(capture) = del_regex.captures(line.as_str()) {
+                println!("Del Key {}", &capture[1]);
+                map.remove(&capture[1]);
+            };
 
             // TODO: understand why I need to pass a reference to println
-            println!("Key {}, Value {}", &capture[1], &capture[2]);
-            map.insert(String::from(&capture[1]), String::from(&capture[2]));
         }
     });
 
@@ -50,7 +55,8 @@ fn main() {
     let map = init_from_logfile("log");
     let values = RefCell::new(map);
     let values_ref = &values;
-    let mut file = upsert_logfile("log");
+    let file = RefCell::new(upsert_logfile("log"));
+    let file_ref = &file;
 
     print_logfile("log");
 
@@ -62,11 +68,12 @@ fn main() {
                 (key: String, value: String) => |key: String, value: String| {
                     println!("Set {} = {}", key, value);
                     let mut map = values_ref.borrow_mut();
+                    let mut file = file_ref.borrow_mut();
 
                     // TODO: how can I avoid cloning the key and value here?
                     map.insert(key.clone(), value.clone());
 
-                    writeln!(&mut file, "{}={}", key, value).unwrap();
+                    writeln!(file, "{}={}", key, value).unwrap();
                     Ok(CommandStatus::Done)
                 }
             },
@@ -89,6 +96,22 @@ fn main() {
                         }
                     }
 
+
+                    Ok(CommandStatus::Done)
+                }
+            },
+        )
+        .add(
+            "DEL",
+            command! {
+                "Delete a value",
+                (key: String) => |key| {
+                    // TODO: replace &key with key
+                    let mut map = values_ref.borrow_mut();
+                    let mut file = file_ref.borrow_mut();
+                    map.remove(&key);
+
+                    writeln!(&mut file, "DEL {}", key).unwrap();
 
                     Ok(CommandStatus::Done)
                 }
